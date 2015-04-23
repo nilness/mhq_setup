b96d0e3e98dd755d4baf08f49181ff0a83f16812
Nick <nilness@gmail.com>
2015-04-23 17:18:30 -0500

    disalbed launchctl lines
    None of them seem to matter.

--- mhq_cleanup.sh
+++ mhq_cleanup.sh
@@ -37,10 +37,17 @@ DAEMON_NAME="com.macqh.mhq_cleanup.plist"
 #
 #########################
 
-rm "/Library/LaunchAgents/${AGENT_NAME}"
+#launchctl stop "${AGENT_NAME}"
+#launchctl unload "/Library/LaunchAgents/${AGENT_NAME}"
+#launchctl remove "${AGENT_NAME}"
 
-launchctl unload "/Library/LaunchDaemons/${DAEMON_NAME}"
-rm "/Library/LaunchDaemons/${DAEMON_NAME}"
+/usr/bin/srm -mf "/Library/LaunchAgents/${AGENT_NAME}" 2>/dev/null
+
+#launchctl stop "${DAEMON_NAME}"
+#launchctl unload "/Library/LaunchDaemons/${DAEMON_NAME}"
+#launchctl remove "${DAEMON_NAME}"
+
+/usr/bin/srm -mf "/Library/LaunchDaemons/${DAEMON_NAME}" 2>/dev/null
 
 #########################
 #
